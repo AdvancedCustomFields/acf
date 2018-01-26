4cdc27d4ad57c7ccfe57f1279896cd65c51dac55
Christian Nolen <cwpnolen@gmail.com>
2018-01-26 13:16:43 -0500

    Updated the link to edit a post to use localized admin_url instead of the domain variable.

--- assets/js/acf-input.js
+++ assets/js/acf-input.js
@@ -9762,17 +9762,13 @@ var acf;
 			e.$el.addClass('disabled');
 			
 			
-			// set domain variable
-			var domain = window.location.protocol + "//" + window.location.host + "/";
-
-
 			// template
 			var html = [
 				'<li>',
 					'<input type="hidden" name="' + this.$input.attr('name') + '[]" value="' + e.$el.data('id') + '" />',
 					'<span data-id="' + e.$el.data('id') + '" class="acf-rel-item">' + e.$el.html(),
 						'<div class="acf-icon-set">',
-							'<a href="' + domain + 'wp-admin/post.php?post=' + e.$el.data('id') + '&action=edit" class="acf-icon -pencil small dark" data-name="edit_item" target="_blank"></a>\n',
+							'<a href="' + acf_wp_data.admin_url + 'post.php?post=' + e.$el.data('id') + '&action=edit" class="acf-icon -pencil small dark" data-name="edit_item" target="_blank"></a>\n',
 							'<a href="#" class="acf-icon -minus small dark" data-name="remove_item"></a>',
 						'</div>',
 					'</span>',
